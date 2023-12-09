82d77257675107541444bede54193a3f44e779fa
Sritik Dash <sritik.dash@sap.com>
2023-12-09 17:28:51 +0530

    Initial Commit

--- src/js/mainpage.js
+++
@@ -1,9 +0,0 @@
-
-document.body.addEventListener('keydown', function(event){
-    if(event.key === "Escape"){
-        window.close();
-    }
-});
-window.addEventListener("DOMContentLoaded", async () => {
-    await bridge.populateCategory();
-});
